99de0eeb535fd261226a5a9e0e364c56ff8746a6
talhaker <taltal.haker@gmail.com>
2018-07-03 14:12:54 +0300

    Add shuffle boxes

--- public/final.js
+++ public/final.js
@@ -5,11 +5,15 @@
 *********************************************/
 // createBoxHTML naintains an array with box sizes,
 // to be used for sorting/shuffling
-let boxSizes = [];
+const boxSizes = [];
 const $boxes = $("#boxes");
 
 let $boxTemplate = $('#box-template').html();
 let template = Handlebars.compile($boxTemplate);
+
+/********************************************* 
+*          ADD BOX
+*********************************************/
 
 let createBoxHTML = (size) => {
     let boxHtml = template({boxSize: size});
@@ -19,53 +23,6 @@ let createBoxHTML = (size) => {
 
     return boxHtml;
 }
-
-
-
-/********************************************* 
-*          SHUFFLE BOXES
-*********************************************/
-
-let jumble = (arr1, arr2) => {
-    // To simplifay, concatenate the two arrays,
-    // and pick the next element serially.
-    // Than insert it into a random position
-    // in the jumbled array
-    let concatArr = arr1.concat(arr2);
-    let jumbleLength = 1;
-    let jumbledArr = [];
-
-    jumbledArr.push(concatArr[0]);
-    for (let ix = 1; ix < concatArr.length; ix++, jumbleLength++) {
-        // Determine where to insert the next element:
-        // Get a random index between 0 and the number of 
-        // elements, already in the array
-        let jumbleIx = getRandomIndex(jumbleLength + 1);
-        if (jumbleIx === jumbleLength) {
-            jumbledArr.push(concatArr[ix]);
-        }
-        else {
-            jumbledArr.splice(jumbleIx, 0, concatArr[ix]);
-        }
-    }
-
-    return jumbledArr
-}
-
-//console.log(jumble(colors, foods));
-
-
-/********************************************* 
-*          SORT BOXES
-*********************************************/
-
-// I chose to implement the merge-sort
-// algirithm, which runs at O(n*Log(n))
-
-
-// let myArray = ["red", "indigo", "white", "teal", "yellow", "bread", "cheese", "cucumber"];
-// let myArray = [10, 17, 2, 5, 70, 15, 4, 8];
-// console.log(mergeSort(myArray));
 
 let handleAddBox = (size) => {
     if (isNaN(parseInt(size))) {
@@ -79,27 +36,71 @@ let handleAddBox = (size) => {
     }
 }
 
+
+/********************************************* 
+*          SORT BOXES
+*********************************************/
+
+// I use the same merge-sort algirithm, implemented
+// in the first part of the exercise
+
 let handleSortBoxes = () => {
     let sortedArray = mergeSort(boxSizes);
-    boxSizes = sortedArray;
+    // No need to update boxSizes array
+    // boxSizes = sortedArray;
     $boxes.empty();
 
     for (let i = 0; i < boxSizes.length; i++) {
-        let boxHtml = template({boxSize: boxSizes[i]});
+        let boxHtml = template({boxSize: sortedArray[i]});
         $boxes.append(boxHtml);
     }
 }
 
 
 
+/********************************************* 
+*          SHUFFLE BOXES
+*********************************************/
+
+let shuffle = () => {
+    // Insert each element in the boxSizes array
+    // into a random position in the jumbled array
+    let jumbleLength = 1;
+    let jumbledArr = [];
+
+    jumbledArr.push(boxSizes[0]);
+    for (let ix = 1; ix < boxSizes.length; ix++, jumbleLength++) {
+        // Determine where to insert the next element:
+        // Get a random index between 0 and the number of 
+        // elements, already in the array
+        let jumbleIx = getRandomIndex(jumbleLength + 1);
+        if (jumbleIx === jumbleLength) {
+            jumbledArr.push(boxSizes[ix]);
+        }
+        else {
+            jumbledArr.splice(jumbleIx, 0, boxSizes[ix]);
+        }
+    }
+
+    return jumbledArr
+}
+
+let handleShuffleBoxes = () => {
+    let shuffledArray = shuffle();
+    // No need to update boxSizes array
+    // boxSizes = shuffledArray;
+    $boxes.empty();
+
+    for (let i = 0; i < boxSizes.length; i++) {
+        let boxHtml = template({boxSize: shuffledArray[i]});
+        $boxes.append(boxHtml);
+    }
+}
 
 
-
-
-
-
-
-
+/********************************************* 
+*          ON-CLICK
+*********************************************/
 
 $('#add-box').click(() => {
     let boxSize = $('#box-size').val();
@@ -108,6 +109,10 @@ $('#add-box').click(() => {
 
 $('#sort-boxes').click(() => {
     handleSortBoxes();
+});
+
+$('#shuffle-boxes').click(() => {
+    handleShuffleBoxes();
 });
 
 
